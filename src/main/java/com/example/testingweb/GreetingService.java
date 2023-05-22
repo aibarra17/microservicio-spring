60519c2007c9d8262a5a41fc0f4a70ce1a71018b
aibarra17 <80056382+aibarra17@users.noreply.github.com>
2023-05-21 22:45:57 -0400

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+	private String password = "DebuAndres";
 	public String greet() {
 		return "Finaliza el curso DevOps Foundation v4 - Andres Ibarra";
 	}
